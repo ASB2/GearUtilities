92e64c53de5777c9b4909dd584a036122ad17620
Nox Amos <iamasb2@gmail.com>
2014-05-29 22:15:22 -0400

    Update

--- ASB2/common/GU/blocks/containers/BlockSpacialProvider/SpacialProviderWrapper.java
+++ ASB2/common/GU/blocks/containers/BlockSpacialProvider/SpacialProviderWrapper.java
@@ -17,38 +17,39 @@ public class SpacialProviderWrapper extends MetadataWrapper {
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float xHit, float yHit, float zHit) {
         
-        TileEntity tile = world.getTileEntity(x, y, z);
+        // TileEntity tile = world.getTileEntity(x, y, z);
         
-//        if (tile != null && tile instanceof IMultiBlockPart && ((IMultiBlockPart) tile).getMultiBlocks().isEmpty()) {
+        // if (tile != null && tile instanceof IMultiBlockPart &&
+        // ((IMultiBlockPart) tile).getMultiBlocks().isEmpty()) {
+        
+        if (player.getHeldItem() == null) {
             
-            if (player.getHeldItem() == null) {
-                
-                switch (world.getBlockMetadata(x, y, z)) {
-                
-                    case 0: {
-                        
-                        UtilEntity.sendChatToPlayer(player, EnumMultiBlockType.STANDARD.createMultiBlock(world, new Vector3i(x, y, z)));
-                        break;
-                    }
+            switch (world.getBlockMetadata(x, y, z)) {
+            
+                case 0: {
                     
-                    case 1: {
-                        
-                        UtilEntity.sendChatToPlayer(player, EnumMultiBlockType.CHEST.createMultiBlock(world, new Vector3i(x, y, z)));
-                        break;
-                    }
-                    case 2: {
-                        
-                        UtilEntity.sendChatToPlayer(player, EnumMultiBlockType.FURNACE.createMultiBlock(world, new Vector3i(x, y, z)));
-                        break;
-                    }
-                    case 3: {
-                        
-                        UtilEntity.sendChatToPlayer(player, EnumMultiBlockType.TANK.createMultiBlock(world, new Vector3i(x, y, z)));
-                        break;
-                    }
+                    UtilEntity.sendChatToPlayer(player, EnumMultiBlockType.STANDARD.createMultiBlock(world, new Vector3i(x, y, z)));
+                    break;
                 }
-                return true;
-//            }
+                
+                case 1: {
+                    
+                    UtilEntity.sendChatToPlayer(player, EnumMultiBlockType.CHEST.createMultiBlock(world, new Vector3i(x, y, z)));
+                    break;
+                }
+                case 2: {
+                    
+                    UtilEntity.sendChatToPlayer(player, EnumMultiBlockType.FURNACE.createMultiBlock(world, new Vector3i(x, y, z)));
+                    break;
+                }
+                case 3: {
+                    
+                    UtilEntity.sendChatToPlayer(player, EnumMultiBlockType.TANK.createMultiBlock(world, new Vector3i(x, y, z)));
+                    break;
+                }
+            }
+            return true;
+            // }
         }
         return false;
     }
